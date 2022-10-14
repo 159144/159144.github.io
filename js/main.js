f23afbd253117937b153c393afea4a1d44ae17ff
159144 <71586523+159144@users.noreply.github.com>
2022-10-14 10:17:11 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'img/mg1.png') {
+		myImage.setAttribute('src', 'img/mg2.png');
+	} else {
+		myImage.setAttribute('src', 'img/mg1.png');
+	}
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('请输入你的名字。');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = '该网站 酷毙了，' + myName;
+}
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = '该网站 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
+
+function setUserName() {
+	let myName = prompt('请输入你的名字。');
+	if (!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem('name', myName);
+		myHeading.textContent = '该网站 酷毙了，' + myName;
+	}
+}
